913ee266d7e2b6970eca54dec64c59579bfe1694
Vítor Holtz <vitorholtz27@gmail.com>
2021-01-28 08:56:27 -0300

    23

--- main.js
+++ main.js
@@ -4,22 +4,45 @@ const todoInput = document.querySelector('#add');
 const todoButton = document.querySelector('.fa-arrow-down');
 const todoList = document.querySelector('.to-do-list');
 
+const taskCountDisplay = document.querySelector('.task-count');
+const listItemCount = document.getElementsByClassName('to-do-list-item');
+
+const showAll = document.querySelector('.show-all');
+const showActive = document.querySelector('.show-active');
+const showCompleted = document.querySelector('.show-completed');
+
+const clearCompleted = document.querySelector('.clear', clearCompletedtasks);
+
+
 
 //event listeners
 
 todoButton.addEventListener ('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
+showAll.addEventListener('click', showAllfunction);
+showActive.addEventListener('click', showActivefunction);
+showCompleted.addEventListener('click', showCompletedfunction);
+clearCompleted.addEventListener ('click', clearCompletedtasks);
 
 //Functions 
 
 function addTodo(event) {
 
     event.preventDefault()
+
+    if (todoInput.value === '') {
+
+      // alert if the user doesn't type anything
+
+      alert('Write something!')
+    } 
+    
+    else { 
     
      // create to-do item
     
      const todoListItem = document.createElement('li');
-     todoListItem .classList.add('to-do-list-item');
+     todoListItem.classList.add('to-do-list-item');
      todoList.appendChild(todoListItem);
 
     // create default checkbutton
@@ -46,7 +69,11 @@ function addTodo(event) {
 
      // clear input value
      todoInput.value = '';
-    
+
+    // Task count info
+    taskCountDisplay.innerHTML = listItemCount.length + ' task(s) left';
+
+    }
     }
 
     function deleteCheck(e) {
@@ -57,13 +84,76 @@ function addTodo(event) {
     if (item.classList[0] === 'fas') {
         const todo = item.parentElement;
         todo.remove();
+
+        taskCountDisplay.innerHTML = listItemCount.length + ' task(s) left';
       }
 
         //check task
     if (item.classList[0] === 'default-checkbox') {
-        const writenTask = document.querySelector('.task');
+        const writenTask = item.parentElement;
         writenTask.classList.toggle('completed');
       }
 
     }
+
+    function showAllfunction () {
+
+      const todotasks = document.querySelectorAll('.to-do-list-item');
+      const todotasksCompleted = document.querySelectorAll('.completed');
+      var i;
+      for (i = 0; i < todotasksCompleted.length; i++) {
+        todotasksCompleted[i].style = 'display: flex'
+        todotasks[i].style = 'display: flex'
+      }
+
+    }
+
+    function showActivefunction () {
+
+      const todotasks = document.querySelectorAll('.to-do-list-item');
+      const todotasksCompleted = document.querySelectorAll('.completed');
+      var i;
+      for (i = 0; i < todotasks.length; i++) {
+        todotasks[i].style = 'display: flex'
+      }
+      for (i = 0; i < todotasksCompleted.length; i++) {
+        todotasksCompleted[i].style = 'display: none'
+      }
+
+    }
+
+    function showCompletedfunction () {
+
+      const todotasks = document.querySelectorAll('.to-do-list-item');
+      const todotasksCompleted = document.querySelectorAll('.completed');
+      var i;
+      for (i = 0; i < todotasksCompleted.length; i++) {
+        todotasksCompleted[i].style = 'display: flex'
+      }
+
+
+    }
+
+    function clearCompletedtasks (e) {
+
+      const todotasks = document.querySelectorAll('.to-do-list-item');
+      const todotasksCompleted = document.querySelectorAll('.completed');
+      var i;
+      for (i = 0; i < todotasksCompleted.length; i++) {
+        todotasksCompleted[i].remove();
+      }
+
+      
+    }
+
+
+
+  
+
+    
+
+
+  
+    
+
 
